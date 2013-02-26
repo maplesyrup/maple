dd310ebcbbecbd368000e88db81bdc7ea1a08dd5
Ben Rudolph <rudolphben@gmail.com>
2013-02-25 17:22:16 -0800

    Initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maple::Application.config.secret_token = '925da5680807c3d863972b9477cc2c785f36e5bf31665c45169c3f06e92d6201bf07dc492f5cfb6e8def044332f4000dee3cb49cecfe9bfb0809211cbfe5514b'
