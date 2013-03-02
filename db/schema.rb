d59a5b03d3a05e73f728caea68403008fbef5a50
Ben Rudolph <rudolphben@gmail.com>
2013-03-01 20:38:49 -0800

    Setup user post relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302031724) do
+ActiveRecord::Schema.define(:version => 20130302043132) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
