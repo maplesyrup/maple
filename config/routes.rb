03a4384bc43e9eb6ed2b685af8c7f096606ff44e
Ben Rudolph <rudolphben@gmail.com>
2013-03-01 22:47:48 -0800

    Can now create new posts by sending image to path. Test included!

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,9 @@ Maple::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users do
+    resources :posts
+  end
 
   # Sample resource route with options:
   #   resources :products do
