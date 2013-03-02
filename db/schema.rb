210c988c74b7db3634facf0965e397e107809c1a
Rahul Pandey <rpandey1234@gmail.com>
2013-03-01 21:50:27 -0800

    fix conflict with schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(:version => 20130302045943) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
