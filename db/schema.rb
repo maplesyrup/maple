5d22b96ffb0e582b945796d79c2e4544af26daed
Ben Rudolph <rudolphben@gmail.com>
2013-03-03 11:05:10 -0800

    Added user paperclip attachment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302053427) do
+ActiveRecord::Schema.define(:version => 20130303183749) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(:version => 20130302053427) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -46,6 +44,10 @@ ActiveRecord::Schema.define(:version => 20130302053427) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
