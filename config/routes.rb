c8ebce42bf00178ad93f479dc62070821da9a84a
Charlie Vuong <cvuong90@gmail.com>
2013-03-02 19:57:36 -0800

    Change the root view

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Maple::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'about#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
