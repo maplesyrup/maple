70e0a85847ef851809bcd86c8c186ecd7e77aa33
Ben Rudolph <rudolphben@gmail.com>
2013-03-10 09:59:25 -0700

    Checking in schema file from migration

--- db/schema.rb
+++ db/schema.rb
@@ -57,4 +57,12 @@ ActiveRecord::Schema.define(:version => 20130310082745) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
+  create_table "votes", :force => true do |t|
+    t.boolean  "is_positive"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
+    t.integer  "post_id"
+  end
+
 end
