a3c9b5111eafd0ed415ac652cddbfc8cfda6dafe
Charlie Vuong <cvuong90@gmail.com>
2013-03-09 18:57:56 -0800

    Added controller action for processing login check and added the route for the controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
   	# get particular user here
   end
 
+  def check_mobile_login
+    print "We are inside check_mobile_login controller"
+    token = params[:token]
+
+    user = FbGraph::User.me(token)
+    user = user.fetch
+
+    logged_in_user = User.find_by_uid(user.identifier)
+
+    print logged_in_user
+
+    respond_to do |format|
+      format.json { render :json => logged_in_user.authentication_token}
+    end
+  end
+
 end
