09204712f54bf9a60226d1dad1560d77392cb5cb
Charlie Vuong <cvuong90@gmail.com>
2013-03-10 17:32:16 -0700

    Added ability for a User to vote up a Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,14 @@ class PostsController < ApplicationController
   def new
     @post = Post.new(params[:post])
     @companies = Company.all
+  end
 
+  def vote_up
+    if user_signed_in?
+      post = Post.find_by_id(params[:post_id])
+      user = User.find_by_id(current_user.id)
 
+      user.vote_for(post)
+    end
   end
 end
