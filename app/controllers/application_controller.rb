185d2de2850ea3356a370de8519a6d4e13c64883
Ben Rudolph <rudolphben@gmail.com>
2013-04-08 21:46:39 -0700

    Load posts in order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery
 
   def home
-    @posts = Post.paged_posts
+    @posts = Post.paged_posts({ :sort => { :by => 'total_votes' } })
     @companies = Company.paged_companies
   end
 end
