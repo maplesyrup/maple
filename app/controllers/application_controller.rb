785300ec1d18827c9b6b0504f038fadddd2ab921
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-04-19 18:57:47 -0700

    removed application/session route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,4 @@ class ApplicationController < ActionController::Base
     @posts = Post.paged_posts({ :sort => { :by => 'total_votes' } })
     @companies = Company.paged_companies
   end
-
-  def sessions
-    render :json => { :user_signed_in? => user_signed_in?,
-                      :company_signed_in? => company_signed_in?,
-                      :current_user => current_user && current_user.public_model.html_safe || nil, 
-                      :current_company => current_company && current_company.public_model.html_safe || nil }
-  end
 end
