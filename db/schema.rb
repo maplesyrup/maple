8b18ffe44e63281f1ea3dd73b19e57b53f964f24
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-04-24 11:21:30 -0700

    added campaign model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416202630) do
+ActiveRecord::Schema.define(:version => 20130424180442) do
+
+  create_table "campaigns", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "starttime"
+    t.datetime "endtime"
+    t.integer  "company_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "companies", :force => true do |t|
     t.string   "name"
