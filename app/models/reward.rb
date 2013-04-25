d07d4cb47a3d15c5cde56ff6cc725e75a2363c83
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-04-24 19:43:41 -0700

    set up many-to-many relationship for users and rewards

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,8 +3,9 @@ class Reward < ActiveRecord::Base
                   :swag_award, :monetary_award
 
   belongs_to :campaign
+  has_and_belongs_to_many :users 
 
   validates :title, :presence => true
   validates :description, :presence => true
-    
+
 end
