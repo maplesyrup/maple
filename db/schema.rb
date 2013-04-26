e7025317b62ff3ba0e88d92ec7284d7574a01b2d
Charlie Vuong <cvuong90@gmail.com>
2013-04-26 16:37:50 -0700

    Mockup top contributors

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130425022232) do
+ActiveRecord::Schema.define(:version => 20130425045808) do
 
-  create_table "campaigns", :force => true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "starttime"
-    t.datetime "endtime"
-    t.integer  "company_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+  create_table "assets", :force => true do |t|
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.integer  "attachable_id"
+    t.string   "attachable_type"
   end
 
   create_table "comments", :force => true do |t|
@@ -53,15 +54,14 @@ ActiveRecord::Schema.define(:version => 20130425022232) do
     t.string   "blurb_body",                :default => "Hey, this is our company!",      :null => false
     t.string   "more_info_title",           :default => "More Info",                      :null => false
     t.string   "more_info_body",            :default => "Here's a little more about us.", :null => false
-    t.string   "splash_image_file_name"
-    t.string   "splash_image_content_type"
-    t.integer  "splash_image_file_size"
-    t.datetime "splash_image_updated_at"
     t.string   "logo_file_name"
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
-    t.string   "iframe_auth_token"
+    t.string   "splash_image_file_name"
+    t.string   "splash_image_content_type"
+    t.integer  "splash_image_file_size"
+    t.datetime "splash_image_updated_at"
   end
 
   create_table "follows", :force => true do |t|
@@ -88,30 +88,6 @@ ActiveRecord::Schema.define(:version => 20130425022232) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "company_id"
-    t.integer  "campaign_id"
-  end
-
-  create_table "relationships", :force => true do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.string   "followed_type"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-  end
-
-  add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
-  add_index "relationships", ["followed_type"], :name => "index_relationships_on_followed_type"
-  add_index "relationships", ["follower_id", "followed_id", "followed_type"], :name => "relationship_index", :unique => true
-  add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
-
-  create_table "rewards", :force => true do |t|
-    t.integer  "campaign_id"
-    t.string   "title"
-    t.text     "description"
-    t.integer  "monetary_reward"
-    t.string   "swag_award"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
   end
 
   create_table "users", :force => true do |t|
