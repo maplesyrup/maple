4cd4facb027282ffbbee66694c52b4fd210ae14f
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-04-28 23:33:52 -0700

    added datetime back to campaign model

--- test/functional/campaigns_controller_test.rb
+++ test/functional/campaigns_controller_test.rb
@@ -16,4 +16,21 @@ class CampaignsControllerTest < ActionController::TestCase
     assert_equal response.length, 2, "incorrect number of campaigns"
   end
   
+  test "campaign create" do
+    response = post :create, :company_id => 1, 
+        :campaign => 
+          {:title => "test", :description => "Yo yo yo", 
+            :starttime => Time.new, :endtime => Time.new}
+   
+    require 'pry'
+    binding.pry
+
+    response = JSON.parse(response.body)       
+    assert_equal response["title"], "test", "incorrect response"
+
+    response = get :index, :company_id => 1
+    response = JSON.parse(response.body)
+
+    assert_equal response.length, 2, "didn't save the campaiga"
+  end 
 end
