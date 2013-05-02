1410c23420353aa3235610156715beaa4698b611
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-05-02 10:43:04 -0700

    more complete tests for upvote rewards

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -20,6 +20,21 @@ class PostsControllerTest < ActionController::TestCase
       
     sign_in users(:two)
     response = post :vote_up, :post_id => @post.id 
+    assert_equal @post.rewards.count, 1, "Post wasn't assigned a reward"
+    assert_equal @post.user.rewards.count, 1, "User wasn't assigned a reward"
+    
+    assert_equal @post.rewards[0].quantity, 0, "Reward quantity not updated"
+    assert_equal @post.plusminus, @post.rewards[0].min_votes, "Post doesn't have enough votes to win this award"
+    
+    response = post :vote_up, :post_id => posts(:one).id    
+    assert_equal posts(:one).rewards.count, 0, "Post shoudn't be allowed to win any award as it doesn't have enough upvotes to qualify"
+
+    sign_out users(:one)
+    sign_in @user 
+    response = post :vote_up, :post_id => posts(:one).id
+    
+    assert_equal posts(:one).rewards.count, 0, "Post shouldn't be able to win any awards the quantity of the award is at 0" 
+    
   end
 
   test "uploads image as post" do
