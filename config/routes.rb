b20c45ee0a3e4a354c82f8a82a691844836c5e1e
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-05-06 11:06:44 -0700

    removed route nesting #headache

--- config/routes.rb
+++ config/routes.rb
@@ -23,12 +23,12 @@ Maple::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   resources :posts
-  resources :companies, :shallow => true do
-    resources :campaigns, :shallow => true, :except => [:new] do
-      resources :rewards, :except => [:new]
-    end
-  end
-  resources :campaigns, :only => :index
+  
+  resources :campaigns 
+  
+  resources :rewards
+  
+  resources :companies
 
   put 'users/follow' => 'users#follow'
   resources :users
